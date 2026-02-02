d6b0e1297e5dc5ae8263fa1e345dbc3ab29154f8
kongjingchun <jing20130808@vip.qq.com>
2026-02-02 14:10:38 +0800

    创建.gitignore

--- test_hello.py
+++ test_hello.py
@@ -6,6 +6,7 @@ from time import sleep
 def test_my_test(page: Page):
     page.goto("https://www.gqkt.cn/")
     page.get_by_role("button", name="登录").click()
+    # 断点调试
     page.pause()
     page.locator("div").nth(3).click()
     page.get_by_role("textbox", name="请输入您的账户").click()
