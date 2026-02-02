e97bad82e707057bcad54f8ee9d20fbceb9574b6
kongjingchun <jing20130808@vip.qq.com>
2026-02-02 18:44:32 +0800

    test

--- test_hello.py
+++ test_hello.py
@@ -1,17 +1,16 @@
-"""Playwright UI 自动化测试 - 使用 pytest 运行。"""
-from playwright.sync_api import Page, expect
-from time import sleep
+from playwright.sync_api import sync_playwright
 
-
-def test_my_test(page: Page):
-    page.goto("https://www.gqkt.cn/")
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("http://47.116.12.183/login.html")
+    page.get_by_label("用 户 名:",exact=True).fill("test")
+    page.get_by_label("密 码:",exact=True).fill("123456")
     page.get_by_role("button", name="登录").click()
-    # 断点调试
+    page.get_by_role('link', name='新增模块').click()
+    page.get_by_label("模块名称:").fill("test")
+    page.get_by_role("listbox").get_by_role("option", name="78119270ead64c1bab153a73f").click()
+    page.get_by_label("模块描述:").fill("test")
+    page.get_by_text('点击提交').click()
     page.pause()
-    page.locator("div").nth(3).click()
-    page.get_by_role("textbox", name="请输入您的账户").click()
-    page.get_by_role("textbox", name="请输入您的账户").fill("superadmin")
-    page.get_by_role("textbox", name="请输入您的账户").press("Tab")
-    page.get_by_role("textbox", name="请输入您的密码").fill("gqkt111!")
-    page.get_by_role("button", name="登录").click()
-    sleep(5)
